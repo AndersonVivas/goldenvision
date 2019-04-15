039335361a3eb1df7d425eddc80b3586c2f0f309
AndersonVivas <vivas251@gmail.com>
2019-04-15 15:31:03 -0500

    configuracion de proyecto actualizado
    se hizo configuraciones de idioma y de el nombre de la app

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace goldenvision\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use goldenvision\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Guayaquil',
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'es',
 
     /*
     |--------------------------------------------------------------------------
@@ -91,7 +91,7 @@ return [
     |
     */
 
-    'fallback_locale' => 'en',
+    'fallback_locale' => 'es',
 
     /*
     |--------------------------------------------------------------------------
@@ -154,11 +154,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        goldenvision\Providers\AppServiceProvider::class,
+        goldenvision\Providers\AuthServiceProvider::class,
+        // goldenvision\Providers\BroadcastServiceProvider::class,
+        goldenvision\Providers\EventServiceProvider::class,
+        goldenvision\Providers\RouteServiceProvider::class,
 
     ],
 
